0746aae876ab463cf8cb3fca100115fbe3d9c9c1
暑哒暑哒 <willbethewitch1992@126.com>
2024-07-22 22:00:54 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <el-menu router>
+      <el-menu-item index="/menu-collects">Menu Collects</el-menu-item>
+      <!-- 其他菜单项 -->
+    </el-menu>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+/* 样式 */
+</style>
